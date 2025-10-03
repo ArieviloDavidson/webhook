fdfacfe3728543e0df5d2471d2d53ea2a032380d
ArieviloDavidson <davidsondodc2106@gmail.com>
2025-10-03 08:46:04 -0300

    init project webhook

---
+++ main.py
@@ -0,0 +1,4 @@
+# O que é um webhook?
+# Um webhook é uma maneira de um aplicativo fornecer informações em tempo real para outros aplicativos.
+# Ele é um "gancho" que permite que um aplicativo envie dados automaticamente para outro aplicativo quando um evento específico ocorre.
+
