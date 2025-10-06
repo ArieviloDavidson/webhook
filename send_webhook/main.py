1661f6aed064f59c9a3b1e4eaf747e20491f471d
ArieviloDavidson <davidsondodc2106@gmail.com>
2025-10-06 15:49:47 -0300

    docs: alterando formato de pastas

---
+++ send_webhook/main.py
@@ -0,0 +1,23 @@
+# O que é um webhook?
+# Um webhook é uma maneira de um aplicativo fornecer informações em tempo real para outros aplicativos.
+# Ele é um "gancho" que permite que um aplicativo envie dados automaticamente para outro aplicativo quando um evento específico ocorre.
+
+# Nesse exemplo, vou usar o site webhook.site para criar um webhook e enviar uma requisição POST para ele.
+
+import requests
+import json
+
+URL = "AQUI VAI SEU URL UNICO" # URL do webhook.site
+
+# Data precisa ser um dicionário no formato json
+dict_data = {
+    "nome": "João",
+    "idade": 30,
+    "cidade": "São Paulo"
+}
+
+dict_data_json = json.dumps(dict_data) # Converte o dicionário para json
+
+response = requests.post(URL, data=dict_data_json)
+print("Status Code:", response.status_code)
+print("Response Body:", response.text)
